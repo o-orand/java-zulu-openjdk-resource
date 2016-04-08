78509da89d41ff8c8aecd70497acb9f92d060dc5
Olivier ORAND <olivier.orand@orange.com>
2016-04-08 16:09:42 +0200

    reactivate check test

--- run-local-test.sh
+++ run-local-test.sh
@@ -3,6 +3,6 @@
 export TMPDIR=/tmp
 DIR=$(dirname $0)
 
-#${DIR}/test/runCheckTest.sh ${DIR}/test/resources ${DIR}/assets
+${DIR}/test/runCheckTest.sh ${DIR}/test/resources ${DIR}/assets
 
 ${DIR}/test/runInTest.sh ${DIR}/test/resources ${DIR}/assets
